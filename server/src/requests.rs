11548066dc2b271394d54543b6007b205dcf1056
lurkny <larsonbm@colostate.edu>
2025-02-02 15:44:00 -0700

    cargo fmt

--- server/src/requests.rs
+++ server/src/requests.rs
@@ -1,17 +1,16 @@
-use serde::{Serialize, Deserialize};
-
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
 pub struct TourRequest {
     boundary_x: f32,
-    boundary_y: f32
+    boundary_y: f32,
 }
 
 impl TourRequest {
     pub fn new(boundary_x: f32, boundary_y: f32) -> Self {
         TourRequest {
             boundary_x,
-            boundary_y
+            boundary_y,
         }
     }
 }
