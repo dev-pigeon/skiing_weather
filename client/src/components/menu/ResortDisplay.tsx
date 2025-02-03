86bafc26dcfb033eca823a72c1d04f073a6deded
Jack Young <jdydev2003@gmail.com>
2025-02-03 16:51:13 -0700

    can load the little sun

--- client/src/components/menu/ResortDisplay.tsx
+++ client/src/components/menu/ResortDisplay.tsx
@@ -5,6 +5,7 @@ import SlopesLifts from "./SlopesLifts";
 import ResortFeatures from "./ResortFeatures";
 import MountainStats from "./MountainStats";
 import WeatherCard from "./WeatherCard";
+import MyImage from "../../assets/Images/sun.png";
 
 interface ResortDisplay {
   currentResort: SkiResort;
@@ -28,6 +29,9 @@ const ResortDisplay = ({ currentResort }: ResortDisplay) => {
       <SlopesLifts currentResort={currentResort} />
       <MountainStats currentResort={currentResort} />
       <ResortFeatures currentResort={currentResort} />
+      <div>
+        <img height={20} width={20} src={MyImage} alt="Description of image" />
+      </div>
     </Stack>
   );
 };
